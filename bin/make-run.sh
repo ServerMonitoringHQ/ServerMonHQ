2b7efef6473fba596f50c2ca77721cc1fd802115
Ian Purton <ian.purton@gmail.com>
2011-10-26 11:08:16 -0400

    Run ob now has a re-start

--- bin/make-run.sh
+++ bin/make-run.sh
@@ -2,14 +2,16 @@
 # make-run.sh 
 # make sure a process is always running.  
 # Add the following to the crontab (i.e. crontab -e)
-# */5 * * * * /home/ianpurton.com/projects/worker-servermonhq/make-run.sh
+# */5 * * * * /home/ianpurton.com/projects/worker-servermonhq/make-run.sh PASSWORD
+
+echo "Password : $1"
 
 process=servermonitoringhq 
-makerun="/home/ianpurton.com/projects/worker-servermonhq/runjob.sh"  
+makerun="$HOME/projects/worker-servermonhq/bin/runjob.exp $1"  
 
 if ps ax | grep -v grep | grep $process > /dev/null         
 then                 
   exit         
 else         
-  $makerun &         
+  $makerun
 fi 
--- runjob.sh
+++
@@ -1,4 +0,0 @@
-export RAILS_ENV=production
-export QUEUE=servermonitoringhq
-
-bundle exec rake resque:work
