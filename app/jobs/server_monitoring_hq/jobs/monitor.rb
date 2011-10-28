751cc1b4ebd8d51742eec997db9cc672024ce8d6
Ian Purton <ian.purton@gmail.com>
2011-10-28 05:55:12 -0400

    Use puts so I can see some output

--- app/jobs/server_monitoring_hq/jobs/monitor.rb
+++ app/jobs/server_monitoring_hq/jobs/monitor.rb
@@ -9,19 +9,19 @@ module ServerMonitoringHQ
       def self.perform(server, return_url, timestamp, env=production)
         check_job_age(timestamp)
 
-        Logger.info "Return URL (#{return_url})"
+        puts "Return URL (#{return_url})"
 
         if !server[:url].blank?
-          Logger.info "Monitor via Agent: #{server[:url]} #{server[:id]} #{server[:env]}"
+          puts "Monitor via Agent: #{server[:url]} #{server[:id]} #{server[:env]}"
           SysStats::Stats.monitor_url(server[:url], server[:id], return_url)
         else
-          Logger.info "Monitor: #{server[:hostname]} #{server[:id]} #{server[:password]} #{server[:username]} #{env}"
+          puts "Monitor: #{server[:hostname]} #{server[:id]} #{server[:password]} #{server[:username]} #{env}"
 
           time = Benchmark.measure do
             SysStats::Stats.monitor(server[:hostname], server[:username], server[:password], server[:port], server[:id], server[:private_key], return_url)
           end
           
-          Logger.info "Monitor Finished: " + time.to_s
+          puts "Monitor Finished: " + time.to_s
         end
       end
       
