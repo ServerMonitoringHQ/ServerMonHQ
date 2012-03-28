71dfcc55ae742687b333640682bb02d833ba84aa
ianpurton <ianpurton@li311-142.members.linode.com>
2012-03-28 09:03:14 -0400

    Tidy up comnments

--- bin/make-run.sh
+++ bin/make-run.sh
@@ -2,7 +2,7 @@
 # make-run.sh 
 # make sure a process is always running.  
 # Add the following to the crontab (i.e. crontab -e)
-# */5 * * * * /home/ianpurton.com/projects/worker-servermonhq/make-run.sh PASSWORD
+# */5 * * * * /home/ianpurton/projects/servermonhq/bin/make-run.sh PASSWORD
 
 process=servermonitoringhq 
 makerun="$HOME/projects/servermonhq/bin/runjob.exp $1"  
