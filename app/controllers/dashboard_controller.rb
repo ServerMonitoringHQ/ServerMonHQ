138fb4c18378c5743b3888d9769322527cac0584
Ian Purton <ian.purton@gmail.com>
2012-04-11 17:15:53 +0100

    Wasn't showing active accounts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def index
   
     @regs = Account.where('trial_end > ? and active = ?', Date.today, false).order('trial_end')
-    @active = Account.where('active > ?', true).order('created_at')
+    @active = Account.where('active = ?', true).order('created_at')
 
     respond_to do |format|
       format.html { render :layout => false }
