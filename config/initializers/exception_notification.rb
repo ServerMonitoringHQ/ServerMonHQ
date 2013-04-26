362e835b538820c110e4e304682a2a627e39b542
Ian Purton <ian.purton@gmail.com>
2013-04-26 05:47:36 -0400

    Stop receiving errors

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,5 +2,5 @@ if Rails.env.production?
   Servermonitoringhq::Application.config.middleware.use ExceptionNotifier,   
     :email_prefix => "[ERROR] ",   
     :sender_address => '"Notifier" <notifier@servermonitoringhq.com>',   
-    :exception_recipients => ['support@servermonitoringhq.com']
+    :exception_recipients => ['support@test.com']
 end
